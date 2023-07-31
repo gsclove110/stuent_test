3b76007d6cdf47425373cdc9ae954e78274e1273
guoshenchao <1158374787@qq.com>
2023-07-31 12:10:41 +0800

    完成项目与数据库的连接，并返回数据库的结果

--- student-system/src/com/itheima/login.java
+++ student-system/src/com/itheima/login.java
@@ -1,9 +1,18 @@
 package com.itheima;
-
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.itheima.polo.student;
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class login {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         while (true) {
             //构建登入界面
             System.out.println("----------------------欢迎来到黑马学生管理系统---------------------------");
@@ -48,8 +57,15 @@ public class login {
     //完成四个功能
 
     //添加学生
-    public static void addStudent(){
+    public static void addStudent() throws Exception {
         System.out.println("添加学生");
+        List<student> list = getList();
+        System.out.println(list);
+
+
+        //键盘录入学生的id
+
+
     }
 
 
@@ -70,5 +86,61 @@ public class login {
     //查询学生
     public static void queryStudent(){
         System.out.println("查询学生");
+    }
+
+
+    //生成一个与mysql数据库连接的方法
+    public static List<student> getList() throws Exception {
+
+        //建立数据库连接
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("C:\\Users\\admin\\IdeaProjects\\student_test01\\student-system\\Druid.properties"));
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+        Connection conn = dataSource.getConnection();
+
+        //2.sql语句 ：查询数据库
+        String sql = "select * from student";
+        //3.创建执行sql语句的对象
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        //4.设置参数
+        //5.执行sql
+
+        ResultSet rs = pstmt.executeQuery();
+
+        //6.处理结果
+
+        //创建集合，用于存储学生数据
+        List<student> list = new ArrayList<>();
+        //清空list集合
+        list.clear();
+
+        while (rs.next()){
+            //创建学生对象
+            student stu = new student();
+            String number = rs.getString("number");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            String address = rs.getString("address");
+
+            //将数据库中的数据封装到学生对象当中去
+            stu.setId(number);
+            stu.setName(name);
+            stu.setAge(age);
+            stu.setAddress(address);
+
+            //将学生数据添加到集合当中去
+            list.add(stu);
+
+        }
+
+        //7.释放资源
+        rs.close();
+        pstmt.close();
+        conn.close();
+
+        return list;
+
+
+
     }
 }
