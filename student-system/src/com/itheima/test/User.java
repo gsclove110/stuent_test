37084d5d5d424328e4cb04a9a7387b423f7a4ea1
guoshenchao <1158374787@qq.com>
2023-07-31 15:45:15 +0800

    生成用户类

---
+++ student-system/src/com/itheima/test/User.java
@@ -0,0 +1,50 @@
+package com.itheima.test;
+
+public class User {
+    private String username;
+    private String password;
+    private String personID;
+    private String phoneNumber;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", personID='" + personID + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
