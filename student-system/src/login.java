665e77cc5e1256931b9fbca8cf1ea189b63ca205
guoshenchao <1158374787@qq.com>
2023-07-31 10:39:49 +0800

    初始化菜单

---
+++ student-system/src/login.java
@@ -0,0 +1,13 @@
+public class login {
+    public static void main(String[] args) {
+        //构建登入界面
+        System.out.println("----------------------欢迎来到黑马学生管理系统---------------------------");
+        System.out.println("1：添加学生");
+        System.out.println("2：删除学生");
+        System.out.println("3：修改学生");
+        System.out.println("4：查询学生");
+        System.out.println("5：退出");
+        System.out.println("请输入您的选择");
+
+    }
+}
